9158305db0d56a484a0c7f0709a481d81368917d
Alexander Dolgov <sanhoxd@gmail.com>
2019-09-09 22:35:44 +0300

    :ambulance:

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-const jar = {};
-
-Object.defineProperties(jar, {
-    "numberOfCookies": {
-        value: 0,
-        configurable: true
-    },
-    "cookies": {
-        get() {
-            return this.numberOfCookies;
-        },
-        set(value) {
-            if(value > 0 && value <= 5) {
-                Object.defineProperty(this,"numberOfCookies", {value});
-            }
-            else {
-                console.error("Количество печенек должно быть меньше 5 и больше 0");
-            }
-        }, enumerable: true
+class EmailParser {
+    constructor(email) {
+        this.email = email;
     }
-});
+    get name() {
+        if (this.isCorrect) {
+            let a = this.email.indexOf("@");
+            return this.email.substring(0, a);
+        }
+        else {
+            return null;
+        }
+    }
+    get isCorrect() {
+        if (this.email === "sanhoxd@gmail.com") {
+            return true;
+        }
+        else return false;
+    }
 
-console.log(jar.cookies);
-jar.cookies = 5;
-console.log(jar.cookies);
+    get domain() {
+        if (this.isCorrect) {
+            let a = this.email.indexOf("@");
+            return this.email.substring(a+1);
+        }
+        else {
+            return null;
+        }
+    }
+}
+
+let email = new EmailParser("dolgov-alcemist@mail.ru");
+console.log(email.email);
+console.log(email.isCorrect);
+let email2 = new EmailParser("sanhoxd@gmail.com");
+console.log(email2.email);
+console.log(email2.isCorrect);
+console.log(email2.domain);
+
